7312b47b395471d9312d9fa5998c25c82df5444d
AnthonyDalke <anthony.dalke@gmail.com>
2021-04-05 07:29:55 -0500

    Added dictionary comprehension to read csv files

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository will house research and work for the Home Credit Default Risk Kaggle competition, located here: https://www.kaggle.com/c/home-credit-default-risk.
--- kaggle_hcdr.py
+++ kaggle_hcdr.py
@@ -2,3 +2,21 @@
 
 import pandas as pd
 import numpy as np
+import os
+
+# Import files
+
+os.chdir('/Users/anthony/Documents/Github/Kaggle_HCDR')
+
+file_dict = {
+    'app_test': 'application_test.csv'
+    , 'app_train': 'application_train.csv'
+    , 'bureau_bal': 'bureau_balance.csv'
+    , 'bureau_hist': 'bureau.csv'
+    , 'cc_bal': 'credit_card_balance.csv'
+    , 'cc_payments': 'installments_payments.csv'
+    , 'pos_hist': 'POS_CASH_balance.csv'
+    , 'app_hist': 'previous_application.csv'
+}
+
+file_dfs = {idx: pd.read_csv(f) for idx, f in file_dict.items()}
