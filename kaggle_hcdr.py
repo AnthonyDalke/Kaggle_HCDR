f4b14c88eee3150fe31bf6fea771678c66f7a2ad
AnthonyDalke <anthony.dalke@gmail.com>
2021-04-21 08:39:28 -0500

    Imported XGBoost.

--- kaggle_hcdr.py
+++ kaggle_hcdr.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import numpy as np
 import os
+from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, auc, balanced_accuracy_score, classification_report, confusion_matrix, roc_curve
 pd.set_option('max_columns', None)
